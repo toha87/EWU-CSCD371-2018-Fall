9d69e387c9ed9735bd86eff917659a4b83e6acea
toha87 <anatoli.railean@gmail.com>
2018-09-27 14:21:58 -0700

    Resolving Conflict

---
+++ Assignment1/Assignment 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    public class ProgramInputOutput
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, whats your name?");
+            String name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
---
+++ Assignment1/Assignment1UnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment1UnitTest
+{
+    [TestClass]
+    public class UnitTestClassForAss1
+    {
+        [TestMethod]
+        public void TestMethodInputOutput()
+        {
+            string myValue = "Anatoli";
+            string expectedOutput = $@">>Hello, whats your name?
+<<{myValue}
+>>Hello {myValue}!";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expectedOutput, Assignment_1.Program.Main);
+        }
+    }
+    
+}
