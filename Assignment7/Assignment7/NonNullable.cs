f88cbb692815b9148d7fb44c78c65677f09521f3
Anatoli <anatoli.railean@gmail.com>
2018-11-08 16:39:51 -0800

    WIP

---
+++ Assignment7/Assignment7/NonNullable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment7UnitTest
+{
+
+    public interface IFactory<T>
+    {
+        T Create();
+    }
+
+    public class NonNullable<T>
+        where T: struct, IComparable
+    {
+       
+        public T Value
+        {
+            get => Value;
+            set
+            {
+                if (value.Equals(null))
+                    throw new ArgumentNullException("Value cannot be null");
+                else
+                    Value = value;
+            }
+        }
+
+    }
+}
---
+++ Assignment7/Assignment7/ResourceManagement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment7UnitTest
+{
+    public class ResourceManagement : IDisposable
+    {
+        public static int COUNT = 0;
+
+        private StreamReader FileReader { get; }
+        public ResourceManagement(string fileName)
+        {
+            
+            FileReader = new StreamReader(fileName);
+            COUNT++;
+        }
+
+        ~ResourceManagement()
+        {
+            Dispose();
+            COUNT--;
+            
+        }
+
+        public void Dispose()
+        {
+            FileReader?.Dispose();
+        }
+    }
+}
